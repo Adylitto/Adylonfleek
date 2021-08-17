8a57e025731f1f87d10b59c22d6f85267bc2f3ac
Adyl ITTO <32968095+Adylitto@users.noreply.github.com>
2021-08-17 12:56:37 +0200

    Update ajax-contact.php

--- ajax-contact.php
+++ ajax-contact.php
@@ -1,7 +1,7 @@
 <?php
 $toEmail = "adyl@ik.me";
 $mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
-if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+if(mail($toEmail, $_POST["message"], $_POST["message"], $mailHeaders)) {
 print "<p class='success'>Contact Mail Sent.</p>";
 } else {
 print "<p class='Error'>Problem in Sending Mail.</p>";
