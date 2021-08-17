ff14aec454898175c26520aee8a1d7ebeb306e3d
Adyl ITTO <32968095+Adylitto@users.noreply.github.com>
2021-08-17 13:13:43 +0200

    form

--- ajax-contact.php
+++ ajax-contact.php
@@ -11,7 +11,7 @@
         $phone = trim($_POST["phone"]);
         $message = trim($_POST["message"]);
         
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
